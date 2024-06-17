c7757ae91362a4fd1ce6c15d492c9e17293d3b1f
jeremy <you@example.com>
2024-06-17 09:59:54 +0200

    BBS actualizada

--- public/BBDD/nutriwave.sql
+++ public/BBDD/nutriwave.sql
@@ -1,22 +1,46 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 17-06-2024 a las 09:58:26
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.3.6
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de datos: `nutriwave`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `carrito`
+--
 
 CREATE TABLE `carrito` (
   `id` int(11) NOT NULL,
-  `id_plato` int(11) NOT NULL,
   `cantidad` int(11) NOT NULL,
   `usuario_id` int(11) NOT NULL,
   `plato_id` int(11) NOT NULL,
-  `id_pedido` int(11) NOT NULL,
-  `precio_total` decimal(10,2) NOT NULL
+  `precio_total` decimal(10,2) NOT NULL,
+  `pedido_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalles_pedido`
+--
 
 CREATE TABLE `detalles_pedido` (
   `id` int(11) NOT NULL,
@@ -26,6 +50,12 @@ CREATE TABLE `detalles_pedido` (
   `cantidad` int(11) NOT NULL,
   `precio_total` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `messenger_messages`
+--
 
 CREATE TABLE `messenger_messages` (
   `id` bigint(20) NOT NULL,
@@ -37,11 +67,33 @@ CREATE TABLE `messenger_messages` (
   `delivered_at` datetime DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedidos`
+--
+
 CREATE TABLE `pedidos` (
   `id` int(11) NOT NULL,
   `usuario_id` int(11) NOT NULL,
-  `fecha_del_pedido` date NOT NULL
+  `fecha_del_pedido` date NOT NULL,
+  `total_precio` decimal(10,2) NOT NULL,
+  `status` varchar(20) NOT NULL DEFAULT 'pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `pedidos`
+--
+
+INSERT INTO `pedidos` (`id`, `usuario_id`, `fecha_del_pedido`, `total_precio`, `status`) VALUES
+(7, 20, '2024-06-16', 14.00, 'completed'),
+(8, 20, '2024-06-16', 36.60, 'completed');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `plato`
+--
 
 CREATE TABLE `plato` (
   `id` int(11) NOT NULL,
@@ -49,73 +101,158 @@ CREATE TABLE `plato` (
   `peso` decimal(10,2) NOT NULL,
   `precio` decimal(10,2) NOT NULL,
   `kcal` int(11) NOT NULL,
-  `disponibilidad` tinyint(1) NOT NULL
+  `disponibilidad` tinyint(1) DEFAULT 1,
+  `image` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `plato`
+--
+
+INSERT INTO `plato` (`id`, `nombre_del_plato`, `peso`, `precio`, `kcal`, `disponibilidad`, `image`) VALUES
+(1, 'Pollo al curry con arroz', 450.00, 7.00, 616, 1, '\\images\\1.jpg'),
+(2, 'Lasaña de pollo', 450.00, 7.00, 589, 1, '\\images\\2.jpg'),
+(3, 'Vacío de ternera a la pimienta', 450.00, 7.95, 666, 1, '\\images\\3.jpg'),
+(4, 'Pollo en salsa de naranja', 450.00, 7.25, 454, 1, '\\images\\4.jpg'),
+(5, 'Alubias con rape y gamba', 450.00, 7.45, 495, 1, '\\images\\5.jpg'),
+(6, 'Spaghetti a la putanesca', 420.00, 6.95, 564, 0, '\\images\\6.jpg'),
+(7, 'Pavo en salsa mostaza', 450.00, 7.25, 486, 0, '\\images\\7.jpg'),
+(8, 'Risotto a la carbonara', 420.00, 6.95, 793, 1, '\\images\\8.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
 
 CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
   `nombre_de_usuario` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `contrasenia` varchar(255) NOT NULL
+  `contrasenia` varchar(255) NOT NULL,
+  `is_verified` tinyint(1) NOT NULL,
+  `roles` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '["ROLE_USER"]' CHECK (json_valid(`roles`))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Volcado de datos para la tabla `usuario`
+--
 
+INSERT INTO `usuario` (`id`, `nombre_de_usuario`, `email`, `contrasenia`, `is_verified`, `roles`) VALUES
+(20, 'jeremy', 'jeremy.morales.ramirez@gmail.com', '$2y$13$/r4Bcj8f3cSsKMsjiPSXDu7BPzAxw744ttUZBuuLeH5WW1xInWfCm', 1, '[\"ROLE_USER\", \"ROLE_ADMIN\"]');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `carrito`
+--
 ALTER TABLE `carrito`
   ADD PRIMARY KEY (`id`),
   ADD KEY `IDX_77E6BED5B0DB09EF` (`plato_id`),
-  ADD KEY `IDX_77E6BED5DB38439E` (`usuario_id`);
+  ADD KEY `IDX_77E6BED5DB38439E` (`usuario_id`),
+  ADD KEY `FK_CARRITO_PEDIDO` (`pedido_id`);
 
+--
+-- Indices de la tabla `detalles_pedido`
+--
 ALTER TABLE `detalles_pedido`
   ADD PRIMARY KEY (`id`),
   ADD KEY `IDX_DBD868FC4854653A` (`pedido_id`),
   ADD KEY `IDX_DBD868FCB0DB09EF` (`plato_id`),
   ADD KEY `IDX_DBD868FCDB38439E` (`usuario_id`);
 
+--
+-- Indices de la tabla `messenger_messages`
+--
 ALTER TABLE `messenger_messages`
   ADD PRIMARY KEY (`id`),
   ADD KEY `IDX_75EA56E0FB7336F0` (`queue_name`),
   ADD KEY `IDX_75EA56E0E3BD61CE` (`available_at`),
   ADD KEY `IDX_75EA56E016BA31DB` (`delivered_at`);
 
+--
+-- Indices de la tabla `pedidos`
+--
 ALTER TABLE `pedidos`
   ADD PRIMARY KEY (`id`),
   ADD KEY `IDX_6716CCAADB38439E` (`usuario_id`);
 
+--
+-- Indices de la tabla `plato`
+--
 ALTER TABLE `plato`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indices de la tabla `usuario`
+--
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
 
+--
+-- AUTO_INCREMENT de la tabla `carrito`
+--
 ALTER TABLE `carrito`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
+--
+-- AUTO_INCREMENT de la tabla `detalles_pedido`
+--
 ALTER TABLE `detalles_pedido`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT de la tabla `messenger_messages`
+--
 ALTER TABLE `messenger_messages`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
+--
+-- AUTO_INCREMENT de la tabla `pedidos`
+--
 ALTER TABLE `pedidos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
+--
+-- AUTO_INCREMENT de la tabla `plato`
+--
 ALTER TABLE `plato`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
+--
+-- Restricciones para tablas volcadas
+--
 
+--
+-- Filtros para la tabla `carrito`
+--
 ALTER TABLE `carrito`
   ADD CONSTRAINT `FK_77E6BED5B0DB09EF` FOREIGN KEY (`plato_id`) REFERENCES `plato` (`id`),
   ADD CONSTRAINT `FK_77E6BED5DB38439E` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`);
 
+--
+-- Filtros para la tabla `detalles_pedido`
+--
 ALTER TABLE `detalles_pedido`
   ADD CONSTRAINT `FK_DBD868FC4854653A` FOREIGN KEY (`pedido_id`) REFERENCES `pedidos` (`id`),
   ADD CONSTRAINT `FK_DBD868FCB0DB09EF` FOREIGN KEY (`plato_id`) REFERENCES `plato` (`id`),
   ADD CONSTRAINT `FK_DBD868FCDB38439E` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`);
 
+--
+-- Filtros para la tabla `pedidos`
+--
 ALTER TABLE `pedidos`
   ADD CONSTRAINT `FK_6716CCAADB38439E` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`);
 COMMIT;
